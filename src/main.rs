4d70fabb47bd1e64ac8bbdff2caa5389f6de6299
Artemciy <artemciy@gmail.com>
2019-11-28 16:24:39 +0300

    Find dart files

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 // [build] cargo build
-// [build] cd ../../komodoDEX && ../log-loc-rs/target/debug/log-loc -m kmd
+// [build] cd ../../komodoDEX && ../log-loc-rs/target/debug/log-loc -vm kmd
 // # [build] cargo install --path .. --force
 // # [build] cd ../../komodoDEX && log-loc -m kmd
 
+#![feature(non_ascii_idents)]
+
+#[macro_use] extern crate fomat_macros;
 #[macro_use] extern crate gstuff;
 
+use glob::glob;
+use gstuff::{now_ms, status_line, status_line_lm, slurp, with_status_line, ISATTY};
 use structopt::StructOpt;
 use std::str::FromStr;
 
@@ -32,12 +37,39 @@ struct Opt {
 
   /// Show progress and patching highlights.
   #[structopt(short = "v", long)]
-  verbose: bool}
+  verbose: bool,
 
-fn komodo_flutter() -> Result<(), String> {
+  /// On terminals display a status line to track the search and patching progress.
+  #[structopt(short = "s", long)]
+  status: bool}
+
+/// Patch `Log.println('$loc', '...')` in Flutter files (*.dart).
+fn komodo_flutter (opt: Opt) -> Result<(), String> {
+  macro_rules! status {($($args: tt)+) => {if opt.status && *ISATTY {
+    if now_ms() - status_line_lm() > 333 {
+      status_line ("log-loc", line!(), fomat! ($($args)+))}}}}
+  macro_rules! verbose {($($args: tt)+) => {
+    if opt.verbose {
+      if opt.status && *ISATTY {
+        with_status_line (&|| {pintln! ($($args)+)})
+      } else {pintln! ($($args)+)}}}}
+
+  status! ("Looking for dart files…");
+  let files: Vec<_> = try_s! (glob ("**/*.dart")) .collect();
+  let filesⁿ = files.len();
+  for (path, idx) in files.into_iter().zip (1..) {
+    let path = try_s! (path);
+    verbose! ((path.display()));
+    if let Some (name) = path.file_name() {
+      if let Some (name) = name.to_str() {
+        status! ((idx) '/' (filesⁿ) ' ' (name) '…')}}
+    let bytes = slurp (&path);
+    if bytes.is_empty() {continue}
+    
+  }
   ERR! ("TBD")}
 
 fn main() {
   let opt = Opt::from_args();
   match opt.mode {
-    Mode::KomodoFlutter => komodo_flutter().unwrap()}}
+    Mode::KomodoFlutter => komodo_flutter (opt) .unwrap()}}
